dd32e8138f9676bd0a1ac7e04df36f1f100b487f
renzy101 <ren_duhaney@hotmail.com>
2017-10-19 20:16:11 -0500

    Adding lab 4 Maze game to to my existing site.

---
+++ maze.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    //Exercise 1
+    document.getElementById('boundary1').addEventListener("mouseover", function() {
+        document.getElementById('boundary1').classList.add('youlose');
+    });
+
+    
+    //Exersise 2
+    var boundaries = document.querySelectorAll(".boundary");
+
+    for (i = 0; i < boundaries.length; i++) {
+        boundaries[i].addEventListener("mouseover", function() {
+            if (st) {
+                for (x = 0; x < boundaries.length; x++) {
+                    boundaries[x].classList.add('youlose');
+                }
+            }
+        });
+    }
+
+    //Exercise 3 (and 4)
+    var st = false;
+    var end = false;
+
+    document.getElementById('start').addEventListener("click", function() {
+        for (x = 0; x < boundaries.length; x++) {
+            boundaries[x].classList.remove('youlose');
+        }
+        document.getElementById("status").innerHTML = 'Move your mouse over the "S" to begin.';
+        end = false
+        st = true;
+    });
+
+    document.getElementById('end').addEventListener("mouseover", function() {
+        if (st && !document.getElementById('boundary1').classList.contains('youlose')) {
+            document.getElementById("status").innerHTML = 'You Win!';
+            end = true;
+            st = false
+        }
+    });
+
+    //Exersise 6
+    document.getElementById('maze').addEventListener("mouseleave", function() {
+        if (st) {
+            end = true;
+            st = false
+            document.getElementById("status").innerHTML = 'You Lose! No Cheating';
+            for (x = 0; x < boundaries.length; x++) {
+                boundaries[x].classList.add('youlose');
+            }
+        }
+    });
+}
